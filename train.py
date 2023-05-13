8538efc098457ae0defb4d977212b28121172c02
Uuganbayar <ugshanyu@gmail.com>
2023-05-13 12:27:08 +0800

    initial commit

---
+++ train.py
@@ -0,0 +1,191 @@
+import os
+import getpass
+import subprocess
+import sys
+import getpass
+
+
+# # Function to install packages
+# def install(package):
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# # Function to handle Hugging Face login
+# def huggingface_login():
+#     api = HfApi()
+
+#     # Ask user for username and password
+#     username = input("Hugging Face username: ")
+#     password = getpass.getpass("Hugging Face password: ")
+
+#     # Get token
+#     token = api.login(username, password)
+
+#     # Save token to Hugging Face folder
+#     folder = HfFolder()
+#     folder.save_token(token)
+
+# # Install packages
+# install("datasets")
+# install("git+https://github.com/huggingface/transformers")
+# install("librosa")
+# install("evaluate")
+# install("jiwer")
+# install("gradio")
+# install("tensorboard")
+# install("--upgrade transformers")
+# install("--upgrade accelerate")
+
+# Import libraries
+from datasets import load_dataset, DatasetDict
+from transformers import WhisperFeatureExtractor, WhisperTokenizer, WhisperProcessor, WhisperForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from datasets import Audio
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+import torch
+import evaluate
+from huggingface_hub import HfApi, HfFolder
+
+# Main function
+def main():
+    # token = getpass.getpass("Hugging Face token: ")
+    # if not token:
+    #     print("Hugging Face token not found. Please set the HUGGINGFACE_TOKEN environment variable.")
+    #     return
+
+    common_voice = DatasetDict()
+    common_voice["train"] = load_dataset("mozilla-foundation/common_voice_13_0", "mn", split="train+validation", use_auth_token=False)
+    common_voice["test"] = load_dataset("mozilla-foundation/common_voice_13_0", "mn", split="test", use_auth_token=False)
+    common_voice = common_voice.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
+    # ... rest of your code ...
+    feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-large-v2")
+    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-large-v2", language="Mongolian", task="transcribe")
+
+
+    input_str = common_voice["train"][0]["sentence"]
+    labels = tokenizer(input_str).input_ids
+    decoded_with_special = tokenizer.decode(labels, skip_special_tokens=False)
+    decoded_str = tokenizer.decode(labels, skip_special_tokens=True)
+
+    print(f"Input:                 {input_str}")
+    print(f"Decoded w/ special:    {decoded_with_special}")
+    print(f"Decoded w/out special: {decoded_str}")
+    print(f"Are equal:             {input_str == decoded_str}")
+
+    processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2", language="Mongolian", task="transcribe")
+    print(common_voice["train"][0])
+    common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
+    print(common_voice["train"][0])
+
+    def prepare_dataset(batch):
+        # load and resample audio data from 48 to 16kHz
+        audio = batch["audio"]
+
+        # compute log-Mel input features from input audio array 
+        batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+
+        # encode target text to label ids 
+        batch["labels"] = tokenizer(batch["sentence"]).input_ids
+        return batch
+
+    common_voice_train = common_voice['train'].map(prepare_dataset, remove_columns=common_voice['train'].column_names)
+    common_voice_test = common_voice['test'].map(prepare_dataset, remove_columns=common_voice['test'].column_names)
+    
+    @dataclass
+    class DataCollatorSpeechSeq2SeqWithPadding:
+        processor: Any
+
+        def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+            # split inputs and labels since they have to be of different lengths and need different padding methods
+            # first treat the audio inputs by simply returning torch tensors
+            input_features = [{"input_features": feature["input_features"]} for feature in features]
+            batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+
+            # get the tokenized label sequences
+            label_features = [{"input_ids": feature["labels"]} for feature in features]
+            # pad the labels to max length
+            labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+
+            # replace padding with -100 to ignore loss correctly
+            labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+
+            # if bos token is appended in previous tokenization step,
+            # cut bos token here as it's append later anyways
+            if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+                labels = labels[:, 1:]
+
+            batch["labels"] = labels
+
+            return batch
+
+
+
+    data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+
+    import evaluate
+
+    metric = evaluate.load("wer")
+
+    def compute_metrics(pred):
+        pred_ids = pred.predictions
+        label_ids = pred.label_ids
+
+        # replace -100 with the pad_token_id
+        label_ids[label_ids == -100] = tokenizer.pad_token_id
+
+        # we do not want to group tokens when computing the metrics
+        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+        label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+
+        wer = 100 * metric.compute(predictions=pred_str, references=label_str)
+
+        return {"wer": wer}
+
+
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
+
+
+    model.config.forced_decoder_ids = None
+    model.config.suppress_tokens = []
+
+
+    training_args = Seq2SeqTrainingArguments(
+        output_dir="./whisper-large-hi",  # change to a repo name of your choice
+        per_device_train_batch_size=16,
+        gradient_accumulation_steps=1,  # increase by 2x for every 2x decrease in batch size
+        learning_rate=1e-5,
+        warmup_steps=500,
+        max_steps=4000,
+        gradient_checkpointing=True,
+        fp16=True,
+        evaluation_strategy="steps",
+        per_device_eval_batch_size=8,
+        predict_with_generate=True,
+        generation_max_length=225,
+        save_steps=1000,
+        eval_steps=1000,
+        logging_steps=25,
+        report_to=["tensorboard"],
+        load_best_model_at_end=True,
+        metric_for_best_model="wer",
+        greater_is_better=False,
+        push_to_hub=False,
+    )
+
+    trainer = Seq2SeqTrainer(
+        args=training_args,
+        model=model,
+        train_dataset=common_voice_train,
+        eval_dataset=common_voice_test,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+        tokenizer=processor.feature_extractor,
+    )
+
+    trainer.train()
+
+
+
+
+# Run main function
+if __name__ == '__main__':
+    main()
